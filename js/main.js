f283c5ece30eb7db16bf69582c00d779a2f1f891
Eugene <nikulnikovz2000@gmail.com>
2020-09-04 13:21:05 +0300

    last fix

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+    $('.about-us__slick').slick({
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        centerMode: true,
+        // variableWidth: true,
+        adaptiveHeight: true
+});
+});
